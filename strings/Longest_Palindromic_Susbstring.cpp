817ae183f8d25d4b1ae285a7aecdd70877747cc7
pkb07815 <45988215+pkb07815@users.noreply.github.com>
2021-07-15 21:02:23 +0530

    Update Longest_Palindromic_Susbstring.cpp

--- strings/Longest_Palindromic_Susbstring.cpp
+++ strings/Longest_Palindromic_Susbstring.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     
+    // brute force method 
+    
+    // ispalindrome to check if the substring is palindrome.
     bool ispalindrome(string s, int low,int high) {
-        
-        
+  
         while(low<high) {
             
             if(s[low] != s[high])
@@ -21,16 +23,23 @@ public:
         
         int _max=0;
         string temp1;
+        
         for(int i=0; i<s.length();i++)
         {
             
             for(int j=0;j<s.length();j++)
             {
+                
+                // substr function to get string part
                 string temp=s.substr(i,j-i+1);
                 
                 cout<<"temp"<<" "<<temp<<endl;
                 if(ispalindrome(temp,0,temp.length()-1))
                    {
+                    // if it is palindrome then check if this substr length 
+                    // is more than previous max. if that is true, then only update the _max
+                    // and temp1.
+                    
                        if(temp.length() > _max)
                        {
                            temp1=temp;
