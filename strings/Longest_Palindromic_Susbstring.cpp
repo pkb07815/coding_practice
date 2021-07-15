11003ef8744508dd361b9b53ff591ce7489eadc8
pkb07815 <45988215+pkb07815@users.noreply.github.com>
2021-07-15 23:09:45 +0530

    Update Longest_Palindromic_Susbstring.cpp

--- strings/Longest_Palindromic_Susbstring.cpp
+++ strings/Longest_Palindromic_Susbstring.cpp
@@ -1,57 +1,9 @@
 class Solution {
 public:
     
-    // BRUTE FORCE
-//     bool ispalindrome(string s, int low,int high) {
-        
-        
-//         while(low<high) {
-            
-//             if(s[low] != s[high])
-//                 return false;
-            
-//             low++;
-//             high--;
-            
-//         }
-        
-//         return true;
-        
-//     }
-//     string longestPalindrome(string s) {
-        
-//         int _max=0;
-//         string temp1;
-//         for(int i=0; i<s.length();i++)
-//         {
-            
-//             for(int j=0;j<s.length();j++)
-//             {
-//                 string temp=s.substr(i,j-i+1);
-                
-//                 cout<<"temp"<<" "<<temp<<endl;
-//                 if(ispalindrome(temp,0,temp.length()-1))
-//                    {
-//                        if(temp.length() > _max)
-//                        {
-//                            temp1=temp;
-                           
-//                            _max = temp.length();
-                           
-//                            cout<<"temp1"<<temp1;
-//                        }
-                       
-                    
-//                    }
-                
-//             }
-//         }
-        
-//         return temp1;
-        
-//     }
+ 
     
-    //EXPAND AROUND CORNER
+    //EXPAND AROUND CORNER  o(n^2) (1)
     
    
     
